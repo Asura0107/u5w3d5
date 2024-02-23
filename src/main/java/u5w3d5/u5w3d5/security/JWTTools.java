3eb5e025cab79aac1fa4f21169b5d453ec21da31
Asura0107 <mircoorosio@gmail.com>
2024-02-23 12:20:48 +0100

    aggiunta la security chain

---
+++ src/main/java/u5w3d5/u5w3d5/security/JWTTools.java
@@ -0,0 +1,44 @@
+package u5w3d5.u5w3d5.security;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import u5w3d5.u5w3d5.entities.User;
+import u5w3d5.u5w3d5.exception.UnauthorizedException;
+
+
+import javax.security.auth.Subject;
+import java.util.Date;
+
+@Service
+public class JWTTools {
+
+    @Value("${jwt.secret}")
+    private String secret;
+
+    public String createToken(User user) {
+        return Jwts.builder()
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 24 * 7))
+                .subject(String.valueOf(user.getId()))
+                .signWith(Keys.hmacShaKeyFor(secret.getBytes()))
+                .compact();
+    }
+
+    public void verifyToken(String token) {
+        try {
+            Jwts.parser().verifyWith(Keys.hmacShaKeyFor(secret.getBytes())).build().parse(token);
+        } catch (Exception ex) {
+            throw new UnauthorizedException("Problemi col token! Effettua di nuovo il login!");
+        }
+    }
+
+    public String extractIdFromToken(String token) {
+        return Jwts.parser().verifyWith(Keys.hmacShaKeyFor(secret.getBytes()))
+                .build().parseSignedClaims(token).getPayload()
+                .getSubject();
+    }
+
+
+}
